666df58b59ce8cd66c1789fb34ab32a8f798e033
Jerry Yu <jerryyu040115@gmail.com>
2025-11-15 16:27:24 +1300

    a

---
+++ src/client/java/com/jyu115/xiuxian/MccultivationmodClient.java
@@ -0,0 +1,10 @@
+package com.jyu115.xiuxian;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MccultivationmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/jyu115/xiuxian/MccultivationmodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jyu115.xiuxian;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MccultivationmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
